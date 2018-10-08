5bb98431e943daf7a218838a1dac19ad3d7b71b0
peti120 <peti59550@gmail.com>
2018-10-08 11:30:19 +0200

    Basic structure added

---
+++ src/main/java/com/kztp/testengine/TestengineApplication.java
@@ -0,0 +1,12 @@
+package com.kztp.testengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestengineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kztp/testengine/TestengineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kztp.testengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestengineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
