dcb17bea4bb04b6117c810c771d72c0fd1d57a00
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-09 10:16:59 +0200

    creator and authorities fields added to user

--- src/main/java/com/kztp/testengine/model/User.java
+++ src/main/java/com/kztp/testengine/model/User.java
@@ -1,6 +1,8 @@
 package com.kztp.testengine.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Users")
@@ -13,6 +15,16 @@ public class User {
     private String email;
     private String rank;
     private boolean enabled;
+    @OneToMany(mappedBy = "creator")
+    private List<Test> tests = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(
+            name = "authorities",
+            joinColumns = @JoinColumn(name = "username", referencedColumnName = "username")
+    )
+    @Column(name = "authority")
+    private List<String> authorities;
 
     public User() {
     }
@@ -63,5 +75,21 @@ public class User {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public List<Test> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<Test> tests) {
+        this.tests = tests;
+    }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+        this.authorities = authorities;
     }
 }
