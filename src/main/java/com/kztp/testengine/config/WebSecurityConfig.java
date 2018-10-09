2f121c67521e5f59fbbf590cd6dba448c3ab9baa
peti120 <peti59550@gmail.com>
2018-10-09 11:45:31 +0200

    Websecurity config implemented

---
+++ src/main/java/com/kztp/testengine/config/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package com.kztp.testengine.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // roles admin allow to access /admin/**
+    // roles user allow to access /user/**
+    // custom 403 access denied handler
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(new NoPopupBasicAuthenticationEntryPoint());
+
+        /* http.authorizeRequests()
+                .antMatchers("/user/**").permitAll()
+                .antMatchers("/**").hasRole("ADMIN");
+        */
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager(
+            @Autowired DataSource dataSource,
+            @Autowired JdbcTemplate jdbcTemplate,
+            @Autowired AuthenticationManager authenticationManager) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setJdbcTemplate(jdbcTemplate);
+        userDetailsManager.setDataSource(dataSource);
+        // Using AuthenticationManager re-authenticates the session on password change.
+        userDetailsManager.setAuthenticationManager(authenticationManager);
+        return userDetailsManager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
