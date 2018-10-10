624b39aff631ad36eeda27cd10836648bfde21ef
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-10 14:36:18 +0200

    authcontroller added

---
+++ src/main/java/com/kztp/testengine/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.kztp.testengine.controller;
+
+import com.kztp.testengine.model.User;
+import com.kztp.testengine.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public User get(Principal principal) {
+        return userService.getUserByEmail(principal.getName());
+    }
+
+    @DeleteMapping("")
+    public void delete(HttpSession session) {
+        session.invalidate();
+    }
+}
