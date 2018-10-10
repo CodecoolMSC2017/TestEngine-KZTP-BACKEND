31eb1539dec3ba9d74e8c53ca150bdb23172ca86
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-10 12:14:35 +0200

    admin/newadmin endpoint implemented

--- src/main/java/com/kztp/testengine/controller/RegisterController.java
+++ src/main/java/com/kztp/testengine/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.kztp.testengine.controller;
 
+import com.kztp.testengine.exception.UnauthorizedRequestException;
 import com.kztp.testengine.model.User;
 import com.kztp.testengine.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,14 @@ public class RegisterController {
         String password = map.get("password");
         String confirmationPassword = map.get("confirmationpassword");
         return userService.createUser(email,username,password,confirmationPassword);
+    }
+
+    @PostMapping("/admin/newadmin")
+    public User addAdmin(@RequestBody Map<String,String> map) throws UnauthorizedRequestException {
+        String email = map.get("email");
+        String username = map.get("username");
+        String password = map.get("password");
+        String confirmationPassword = map.get("confirmationpassword");
+        return userService.createAdmin(email,username,password,confirmationPassword);
     }
 }
