9141916550ea33ab53d5c614a3ac33b7d79fdecc
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-10 12:44:45 +0200

    basic usercontroller implemented

---
+++ src/main/java/com/kztp/testengine/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.kztp.testengine.controller;
+
+import com.kztp.testengine.model.User;
+import com.kztp.testengine.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/change-password")
+    public void changePassword(@RequestBody Map<String,String> map) {
+        User user = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        String oldPassword = map.get("oldpassword");
+        String newPassword = map.get("newpassword");
+        userService.changePassword(user.getId(),oldPassword,newPassword);
+    }
+
+    @GetMapping(path = "/{username}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getUserByUsername(@PathVariable("username")String username){
+        return userService.getUserByUsername(username);
+    }
+
+
+}
