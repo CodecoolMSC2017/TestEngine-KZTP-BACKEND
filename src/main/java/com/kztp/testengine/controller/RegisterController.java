1b7f7b12908839cadfefaad11f4803f20f709d77
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-10 11:11:43 +0200

    register controller implemented

---
+++ src/main/java/com/kztp/testengine/controller/RegisterController.java
@@ -0,0 +1,26 @@
+package com.kztp.testengine.controller;
+
+import com.kztp.testengine.model.User;
+import com.kztp.testengine.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RegisterController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User add(@RequestBody Map<String,String> map) {
+        String email = map.get("email");
+        String username = map.get("username");
+        String password = map.get("password");
+        String confirmationPassword = map.get("confirmationpassword");
+        return userService.createUser(email,username,password,confirmationPassword);
+    }
+}
