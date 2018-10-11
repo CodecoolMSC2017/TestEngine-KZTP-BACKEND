bd2d88c6e432bd08dd11bc14867d1a02b4f94655
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-11 12:11:14 +0200

    createTest method implemented

--- src/main/java/com/kztp/testengine/service/TestService.java
+++ src/main/java/com/kztp/testengine/service/TestService.java
@@ -1,16 +1,26 @@
 package com.kztp.testengine.service;
 
+import com.kztp.testengine.model.Question;
 import com.kztp.testengine.model.Test;
 import com.kztp.testengine.repository.TestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public final class TestService {
     @Autowired
     private TestRepository testRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private XMLService xmlService;
 
     public Test getTestById(int id){
         return testRepository.findById(id);
@@ -22,5 +32,16 @@ public final class TestService {
 
     private Test addTestToDatabase(Test test){
         return testRepository.save(test);
+    }
+
+    public Test createTest(String fileName, int price, int maxPoints, List<Question> questions) {
+        xmlService.createXml(maxPoints,fileName,questions);
+        Test test = new Test();
+        test.setPath(fileName);
+        test.setPrice(price);
+        test.setMaxPoints(maxPoints);
+        test.setCreator(userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName()));
+        addTestToDatabase(test);
+        return test;
     }
 }
