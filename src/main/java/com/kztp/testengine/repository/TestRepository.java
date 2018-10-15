26b2378bbf12a5090b1eb74743e98454f963850b
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-15 14:08:23 +0200

    findByLiveTrue and findByLiveFalse methods added

--- src/main/java/com/kztp/testengine/repository/TestRepository.java
+++ src/main/java/com/kztp/testengine/repository/TestRepository.java
@@ -1,10 +1,16 @@
 package com.kztp.testengine.repository;
 
 import com.kztp.testengine.model.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TestRepository extends JpaRepository<Test,Integer> {
     Test findById(int id);
+    Page<Test> findByLiveTrue(Pageable pageable);
+    Page<Test> findByLiveFalse(Pageable pageable);
 }
