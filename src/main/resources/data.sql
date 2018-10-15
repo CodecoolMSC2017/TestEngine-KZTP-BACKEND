521ec0654bbfce04000883e9ff8f35ebace34366
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-15 13:13:08 +0200

    added tests to data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,47 @@ INSERT INTO public.users(
 INSERT INTO public.authorities(
 	username, authority)
 	VALUES ('admin', 'ROLE_ADMIN');
+
+
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 3 basics', 'more nagyon kígyó', 'pyhton10334k124ion124n12421ssqw', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 2 basics', 'more nagyon kígyó', '134cr23rd32dr2dr2drd23r', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 2.1 basics', 'more nagyon kígyó', 'r23r3ccrc2tcjmrj', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 3.1 basics', 'more nagyon kígyó', 'm923cej92jme293je92', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 3.5 basics', 'more nagyon kígyó', 'cmijjeco3o4o2423434', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 3.7 basics', 'more nagyon kígyó', 'c3r3r2ra2r323r2r23r3r', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 3.2 basics', 'more nagyon kígyó', 'r23crr23crrcr23cr23cr3r23', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 2.6 basics', 'more nagyon kígyó', 'c3rr3rcr2323r34rfft34', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 3.4 basics', 'more nagyon kígyó', 't43ct43t34trcfvwrwrvw', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 2.8 basics', 'more nagyon kígyó', '32v35h5hrwdf2cr3r23r23r', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('Python 2.9 basics', 'more nagyon kígyó', 'r23v3443rnutrbenzsese', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('.net basics', 'c fence', 'wqceqr12r3r2r', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('java basics', 'asd', 'c23cr3r23rc23cr23r', 5, 10, 1, true, 'language');
+INSERT INTO public.tests(
+	title, description, path, price, max_points, creator, enabled, type)
+	VALUES ('javascript basics', 'asdsafasf', '23cr23r23ctc3ta', 5, 10, 1, true, 'language');
