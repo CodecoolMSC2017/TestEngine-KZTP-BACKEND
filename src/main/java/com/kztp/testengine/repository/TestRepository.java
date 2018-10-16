d10f0cf098be713ac96ef68e253afb6b462b38fc
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-16 10:20:48 +0200

    findByCreatorAndLiveTrue method implemented

--- src/main/java/com/kztp/testengine/repository/TestRepository.java
+++ src/main/java/com/kztp/testengine/repository/TestRepository.java
@@ -1,6 +1,7 @@
 package com.kztp.testengine.repository;
 
 import com.kztp.testengine.model.Test;
+import com.kztp.testengine.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface TestRepository extends JpaRepository<Test,Integer> {
     Test findById(int id);
     Page<Test> findByLiveTrue(Pageable pageable);
     Page<Test> findByLiveFalse(Pageable pageable);
+    List<Test> findByCreatorAndLiveTrue(User creator);
 }
