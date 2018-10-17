d5efae0edba258f031c115bf1365e7d760c0fbe8
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-10-17 10:17:32 +0200

    added testratingservice and implemented rate method

---
+++ src/main/java/com/kztp/testengine/service/TestRatingService.java
@@ -0,0 +1,53 @@
+package com.kztp.testengine.service;
+
+import com.kztp.testengine.exception.InvalidVoteException;
+import com.kztp.testengine.model.Test;
+import com.kztp.testengine.model.TestRating;
+import com.kztp.testengine.model.User;
+import com.kztp.testengine.repository.TestRatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRatingService {
+
+    @Autowired
+    private TestRatingRepository testRatingRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TestService testService;
+
+    @Autowired
+    private UsersTestService usersTestService;
+
+    public void rate(int vote,int testId) throws InvalidVoteException {
+        Test test = testService.getTestById(testId);
+        User user = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
+        if(!test.isLive()) {
+            throw new InvalidVoteException("This isn't live yet,you can't rate it.");
+        }
+        if(vote > 5 || vote < 1) {
+            throw new InvalidVoteException("Rating is allowed only between 1 and 5.");
+        }
+
+        if(testRatingRepository.existsByTestAndVoter(test,user)) {
+            TestRating testRating = testRatingRepository.findByTestAndVoter(test,user);
+            testRating.setVote(vote);
+            testRatingRepository.save(testRating);
+        }
+        else {
+            TestRating testRating = new TestRating();
+            testRating.setTest(test);
+            testRating.setVoter(user);
+            testRating.setVote(vote);
+            testRatingRepository.save(testRating);
+        }
+
+
+    }
+}
