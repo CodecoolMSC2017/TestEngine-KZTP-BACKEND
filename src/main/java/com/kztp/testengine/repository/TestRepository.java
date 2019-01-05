1e15bf7b56524b075d5fa711042e8ec1d2cc3d29
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2019-01-05 10:08:27 +0100

    return type of methods changed

--- src/main/java/com/kztp/testengine/repository/TestRepository.java
+++ src/main/java/com/kztp/testengine/repository/TestRepository.java
@@ -15,8 +15,8 @@ public interface TestRepository extends JpaRepository<Test,Integer> {
     Test findById(int id);
     Page<Test> findByLiveTrue(Pageable pageable);
     Page<Test> findByLiveFalse(Pageable pageable);
-    List<Test> findByCreatorAndLiveTrue(User creator);
-    List<Test> findByCreatorAndLiveFalse(User creator);
+    Page<Test> findByCreatorAndLiveTrue(User creator,Pageable pageable);
+    Page<Test> findByCreatorAndLiveFalse(User creator,Pageable pageable);
     @Query(value="SELECT * FROM tests WHERE live=?1 AND LOWER(title) LIKE LOWER(?2) AND rating BETWEEN ?3 AND ?4 AND price BETWEEN ?5 AND ?6 ",nativeQuery = true)
     Page<Test> findAllByParameter(boolean live,String title,int ratingMin,int ratingMax,int priceMin,int priceMax,Pageable pageable);
 }
